fd6163bd0dbc1fa902fc341f45626bb8bd147f72
onebone <jyc00410@gmail.com>
2016-11-02 23:27:45 +0900

    Compatible to latest Nukkit

--- src/main/java/me/onebone/economyshop/ItemDisplayer.java
+++ src/main/java/me/onebone/economyshop/ItemDisplayer.java
@@ -54,7 +54,8 @@ public class ItemDisplayer{
 	
 	public void spawnTo(Player player){
 		AddItemEntityPacket pk = new AddItemEntityPacket();
-		pk.eid = this.eid;
+		pk.entityUniqueId = eid;
+		pk.entityRuntimeId = eid;
 		pk.item = item;
 		pk.speedX = pk.speedY = pk.speedZ = 0;
 		pk.x = (float) pos.x + 0.5F;
